5dc94a0ef824f5429fff9a49360b775607ccc4a6
JCValdezhehe <202210293@gordoncollege.edu.ph>
2024-09-16 17:00:30 +0800

    Svelte head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>import "../app.css";</script>
+<svelte:head>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
-
+</svelte:head>
 <slot></slot>
