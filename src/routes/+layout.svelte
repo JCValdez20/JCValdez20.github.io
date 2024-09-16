d9624a6442c77c198dfe7ca847e9f4b3d7921211
JCValdezhehe <202210293@gordoncollege.edu.ph>
2024-09-16 15:45:23 +0800

    wew

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,7 @@
-<script>import "../app.css";</script><slot></slot>
+<script>import "../app.css";</script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+
+<slot></slot>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from "tailwindcss";
 
+import defaultTheme from "tailwindcss/defaultTheme";
+
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
 
-  theme: {
-    extend: {}
+  extend: {
+    fontFamily: {
+      sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+    },
   },
-
-  plugins: [require("@tailwindcss/typography")]
+  plugins: []
 } as Config;
