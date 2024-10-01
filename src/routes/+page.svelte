e876534ed7256d52c025cdfea9ec253679ea2191
JCValdezhehe <202210293@gordoncollege.edu.ph>
2024-10-01 13:55:39 +0800

    Portfolio Progression

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <div class="w-full max-w-2xl grid md:grid-cols-2 gap-6 p-6 bg-white shadow-lg rounded-lg">
 	<div class="flex justify-center">
-		<img src="jedd.jpg" alt="awesome jedd" class="w-full h-auto max-w-xs rounded-lg object-cover shadow-md" />
+		<img src="" alt="" class="w-full h-auto max-w-xs rounded-lg object-cover shadow-md" />
 	</div>
 	<div class="flex flex-col gap-4 justify-center text-gray-800">
 		<h1 class="text-4xl text-primary-300 font-bold">Welcome!</h1>
 		<p class="text-lg leading-relaxed">
-			Hi! I'm <span class="font-semibold">John Christian V. Valdez</span>, I am a college student in Gordon College
+			Hello! I'm <span class="font-semibold">John Christian V. Valdez</span>, I am a college student in Gordon College
 		</p>
 		<p class="text-lg leading-relaxed">
            I am currently an Information Technology student at Gordon College. I am passionate about exploring the dynamic world of technology and its impact on various industries. At Gordon, I have the opportunity to delve into subjects such as software development, networking, and cybersecurity, which I find both challenging and rewarding.
